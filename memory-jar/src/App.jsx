7943851b3ff4b4bcdc98b8bd29719697424f88c1
Vanshika <vanshikas2018@gmail.com>
2025-01-07 18:35:51 +0530

    Auth login and signup sighout -- implemented with supabase and oauth

--- memory-jar/src/App.jsx
+++ memory-jar/src/App.jsx
@@ -1,18 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { supabaseUrl, supabaseAnonKey } from "/SupabaseClient.js";
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function App() {
-  const [count, setCount] = useState(0);
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+  };
 
-  return (
-    <>
-      <main>
-        <h1 className="h-1 text-red-700">Memory Jar</h1>
-      </main>
-    </>
-  );
+  const signUp = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  };
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <button onClick={signUp}>Sign in with Google</button>;
+  } else {
+    return (
+      <div>
+        <h2>Welcome,{session?.user?.email}</h2>
+        <button onClick={signOut}>Sign Out</button>
+      </div>
+    );
+  }
 }
 
 export default App;
